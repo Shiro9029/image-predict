45c3059e86452bdeaa3bd2b71fb3630b60fba5b7
Shiro9029 <simer.bios9029@gmail.com>
2024-11-27 11:52:53 +0900

    簡単なレスを返すAPIを作成

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,12 @@
-# ベースイメージを指定
 FROM python:3.9-slim
 
-# 作業ディレクトリを設定
 WORKDIR /app
 
-# 必要なファイルをコピー
 COPY requirements.txt requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-# ポートを公開
 EXPOSE 5000
 
-# コンテナ起動時のコマンド
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+import flask
+import joblib
+import numpy as np
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+model = None
 
 @app.route('/')
-def home():
-    return "Hello, Dockerized Flask!"
+def load_model():
+    global model
+    print(" * Loading pre-trained model ...")
+    model = joblib.load("./trained-model/sample-model.pkl")
+    print(' * Loading end')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    response = {
+        "success": False,
+        "Content-Type": "application/json"
+    }
+    if flask.request.method == "POST":
+        if flask.request.get_json().get("feature"):
+            feature = flask.request.get_json().get("feature")
+
+            feature = np.array(feature).reshape((1, -1))
+
+            response["prediction"] = model.predict(feature).tolist()
+
+            response["success"] = True
+    return flask.jsonify(response)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    load_model()
+    print(" * Flask starting server...")
+    app.run()
+    # app.run(host='0.0.0.0', port=5000)
